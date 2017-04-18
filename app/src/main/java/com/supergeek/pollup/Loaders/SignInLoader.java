c39ceaaeda917d3a8bb412005dc20cd531d9c758
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-04-19 02:05:33 +0530

    signin

--- app/src/main/java/com/supergeek/pollup/Loaders/SignInLoader.java
+++ app/src/main/java/com/supergeek/pollup/Loaders/SignInLoader.java
@@ -1,21 +1,118 @@
 package com.supergeek.pollup.Loaders;
 
-import android.content.AsyncTaskLoader;
-import android.content.Context;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.supergeek.pollup.MainActivity;
 import com.supergeek.pollup.models.SignInModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 /**
  * Created by Junejas on 4/19/2017.
  */
 
 public class SignInLoader extends AsyncTaskLoader<SignInModel> {
-    public SignInLoader(Context context) {
+    String ur="http://geekyboy.16mb.com/pollsignin.php";
+    String response;
+    SignInModel model;
+    URL url;
+    Context context;
+    public static int success;
+    public SignInLoader(Context context,SignInModel model) {
         super(context);
+        this.context=context;
+        this.model=model;
     }
 
     @Override
     public SignInModel loadInBackground() {
+        try {
+            //response=MyLoader.makerequest(new URL(url));
+            Log.e("abcd","1");
+            url=new URL(ur);
+            InputStream inputstream;
+            HttpURLConnection connection;
+            connection=(HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setReadTimeout(10000);
+            connection.setReadTimeout(15000);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("email", model.getEmail())
+                    .appendQueryParameter("password", model.getPassword());
+            String query = builder.build().getEncodedQuery();
+
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(query);
+            writer.flush();
+            writer.close();
+            os.close();
+
+            connection.connect();
+
+            inputstream=connection.getInputStream();
+            response=readfromstream(inputstream);
+            checkstatus();
+            Log.e("abcd","2");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("abcd",e.getMessage());
+        }
         return null;
+    }
+    public  String readfromstream(InputStream inputstream)  {
+        Log.e("abcd","103");
+        StringBuilder string=new StringBuilder();
+        if(inputstream!=null) {
+            InputStreamReader inputreader = new InputStreamReader(inputstream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputreader);
+            String line = null;
+            try {
+                line = reader.readLine();
+                while (line != null) {
+                    string.append(line);
+                    line = reader.readLine();
+                }
+                Log.e("abcd","200");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("abcd","201");
+            }
+
+        }
+        return string.toString();
+
+    }
+    public void checkstatus(){
+        try {Log.e("abcdef",response);
+            JSONObject jsonObject=new JSONObject(response);
+             success=jsonObject.getInt("success");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("abcd",e.getMessage());
+        }
     }
 }
--- app/src/main/java/com/supergeek/pollup/SignInActivity.java
+++ app/src/main/java/com/supergeek/pollup/SignInActivity.java
@@ -1,30 +1,70 @@
 package com.supergeek.pollup;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.supergeek.pollup.Loaders.SignInLoader;
+import com.supergeek.pollup.models.SignInModel;
 
 public class SignInActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks{
-
+AutoCompleteTextView textView;
+    EditText text;
+    String email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        setup();
     }
 
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-        return null;
+        getValues();
+        return new SignInLoader(this,new SignInModel(email,password));
     }
 
     @Override
     public void onLoadFinished(Loader loader, Object data) {
-
+            if(SignInLoader.success==1){
+                Intent intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+            }
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
 
+    }
+    public void setup(){
+        textView=(AutoCompleteTextView)findViewById(R.id.email);
+        text=(EditText)findViewById(R.id.password);
+
+    }
+    public void signin(View view){
+        ConnectivityManager connectivity=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo network=connectivity.getActiveNetworkInfo();
+        if(network!=null&&network.isConnected())
+        {
+            try { Log.e("abcd","8");
+                LoaderManager loaderManager = getSupportLoaderManager();
+                loaderManager.restartLoader(1, null, this).forceLoad();
+            }
+            catch (Exception e){}
+
+        }
+    }
+    public void getValues(){
+        email=textView.getText().toString();
+        password=text.getText().toString();
     }
 }
